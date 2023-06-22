0020198dca1617721c10c0c97c7dcf898fcb246e
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-06-22 09:13:45 -0300

    reproducible example

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import WebView from "react-native-webview";
+
+const uri =
+  "https://jitsi.rocket.chat/RocketChat64941e2ec3b1c902672dd182#config.callDisplayName=%22asdas%20dasd%20asd%20asd%20asd%20%22&config.startWithAudioMuted=false&config.startWithVideoMuted=true&userInfo.displayName=%22Daniel%20Silva%22&config.prejoinPageEnabled=false&config.prejoinConfig.enabled=false&config.disableDeepLinking=true";
+
+// to bypass the warning that the browser is not supported
+const userAgent =
+  Platform.OS === "ios"
+    ? "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1"
+    : "Mozilla/5.0 (Linux; Android 12; SM-A315G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        style={styles.container}
+        source={{ uri: uri.replace(/"/g, "'") }}
+        userAgent={userAgent}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
